8c12caa8896f74a1308861560c88bcadd538fe2c
alberto <alberto.soto@gmail.com>
2021-02-03 19:11:01 +0100

    feat: orm nosql service

--- src/main/java/com/fbmoll/teaching/dataaccess/controller/SandboxController.java
+++ src/main/java/com/fbmoll/teaching/dataaccess/controller/SandboxController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SandboxController {
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Model model) {
         model.addAttribute("name", name);
         return "index";
     }
